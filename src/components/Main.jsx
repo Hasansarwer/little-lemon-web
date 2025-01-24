90456ae9fcb9a5c3fc48d061518c99f8b5140dc3
Hasansarwer <hasanmathju@gmail.com>
2025-01-24 16:08:12 +0600

    main and navigation complete

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,20 @@
+import restaurentFood from '/restauranfood.jpg';
+
 export default function Main() {
     return(
-        <main>
-
+        <main >
+            <div className="main">
+                <div className="hero">
+                    <div className="details">
+                        <h1>Little Lemon</h1>
+                        <h2>Chicago</h2>
+                        <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                        <button type="button"> Reserve a Table</button>
+                    </div>
+                </div>
+            </div>
+            
+            <img className='hero-img' src={restaurentFood} alt="" />
         </main>
     )
 };
