65a92e7dbd35d736c1cf4c386cc5415af943b9ee
Hasansarwer <hasanmathju@gmail.com>
2025-01-26 01:30:40 +0600

    adding cards

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,7 @@
 import restaurentFood from '/restauranfood.jpg';
+import bruchetta from '/bruchetta.svg';
+import greekSalad from '/greek salad.jpg'
+import lemon from '/lemon dessert.jpg'
 
 export default function Main() {
     return(
@@ -15,7 +18,45 @@ export default function Main() {
                         <img className='hero-img' src={restaurentFood} alt="" />
                     </div>   
                 </div>
-                
+                <section className="menu">
+                    <div className="special">
+                        <h2>This weeks specials!</h2>
+                        <button type="button">Menu</button>
+                    </div>
+                    <div className="cards">
+                        <div className="card">
+                            <img src={greekSalad} alt="" />
+                            <div className="info">
+                            <div className="heading">
+                                <h3>Greek Salad</h3>
+                                <p>$12.99</p>
+                            </div>
+                            <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src={bruchetta} alt="" />
+                            <div className="info">
+                            <div className="heading">
+                                <h3>Bruchetta</h3>
+                                <p>$5.99</p>
+                            </div>
+                            <p>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. </p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src={lemon} alt="" />
+                            <div className="info">
+                            <div className="heading">
+                                <h3>Lemon Dessert</h3>
+                                <p>$5.00</p>
+                            </div>
+                            <p>This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+                            <h3> Order a delivery </h3>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
             
             
