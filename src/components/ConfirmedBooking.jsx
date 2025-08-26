08a800bd0c83e4794a3daec5116c43deb3a2d9b2
Hasan Sarwer <hasanmathju@gmail.com>
2025-08-27 03:30:51 +0600

    ConfirmedBooking page done

--- src/components/ConfirmedBooking.jsx
+++ src/components/ConfirmedBooking.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 
 export default function ConfirmedBooking() {
+    const bookingData = useLocation().state;
   return (
-    <div className='row'>ConfirmedBooking</div>
+    <div className='confirmed-booking'>
+        <h2 className='row'>Your Booking is Confirmed!</h2>
+        <p>Details:</p>
+        <ul>
+            <li>Name: {bookingData.name}</li>
+            <li>Email: {bookingData.email}</li>
+            <li>Date: {bookingData.date}</li>
+            <li>Time: {bookingData.time}</li>
+            <li>Guests: {bookingData.guests}</li>
+            <li>Occasion: {bookingData.occasion}</li>
+        </ul>
+    </div>
   )
 }
